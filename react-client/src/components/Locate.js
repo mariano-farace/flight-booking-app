efb82f5a937768c7f155b721c8a5e2bd538e5e4c
Mariano <mariano_farace@hotmail.com>
2022-05-08 16:42:56 +0200

    Client First Commit

---
+++ react-client/src/components/Locate.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import TextInput from "./TextInput";
+import LocationSelect from "./LocationSelect";
+
+function Locate(props) {
+  const [value, setValue] = useState("");
+  const [locations, setLocations] = useState([]);
+
+  const submit = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:5000/city-and-airport-search/${value}`)
+      .then((response) => {
+        console.log("response: ", response);
+        return response.json();
+      })
+      .then((json) => {
+        console.log("json: ", json);
+        setLocations(json.data);
+      });
+  };
+
+  return (
+    <div>
+      <TextInput
+        onSubmit={submit}
+        display={props.display}
+        onChange={(e) => setValue(e.target.value)}
+        value={value}
+      />
+      <LocationSelect data={locations} handleChoice={props.handleChoice} />
+    </div>
+  );
+}
+
+export default Locate;
---
+++ react-client/src/components/LocationSelect.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function LocationSelect(props) {
+  const options = props.data.map((location) => (
+    <React.Fragment key={location.id}>
+      <input
+        type="radio"
+        id={location.iataCode}
+        name="select"
+        value={location.iataCode}
+      />
+      <label htmlFor={location.iataCode}>
+        {location.name + ": " + location.iataCode}
+      </label>
+      <br></br>
+    </React.Fragment>
+  ));
+
+  return (
+    <div>
+      {props.data.length > 0 && (
+        <form onChange={(e) => props.handleChoice(e.target.value)}>
+          {options}
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default LocationSelect;
